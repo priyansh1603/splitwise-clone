a90311524916122c5ffcc757c7a322c037fbe939
Priyansh Sharma <sharmapriyansh2000@gmail.com>
2026-04-07 22:09:43 +0530

    Create transporter inside function to use runtime env vars

--- server/utils/sendInvite.js
+++ server/utils/sendInvite.js
@@ -1,14 +1,13 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 async function sendInviteEmail(toEmail, groupName, inviterName) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
   await transporter.sendMail({
     from: `"Splitwise Clone" <${process.env.EMAIL_USER}>`,
     to: toEmail,
